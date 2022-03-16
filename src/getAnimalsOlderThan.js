63d73e2bd2af011d70f049b85ea7d82203bb205c
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-03-16 14:48:49 -0300

    req2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const filtrarEspecie = (especie) => especie.name === animal;
+
+  const arrDeAnimaisDaEspecieBuscada = data.species.find(filtrarEspecie).residents;
+
+  const testarSeAnimaisTemIdadeMinima = (animalTestado) => animalTestado.age > age;
+
+  return arrDeAnimaisDaEspecieBuscada.every(testarSeAnimaisTemIdadeMinima);
 }
 
 module.exports = getAnimalsOlderThan;
