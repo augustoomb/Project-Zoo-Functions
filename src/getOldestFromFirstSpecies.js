1d832130dddcaae47a95b1af9a39be7beec69c13
Augusto Barbosa <augustobarbosa1988@gmail.com>
2022-03-17 11:00:44 -0300

    req9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const buscarEmpregado = (empregado) => empregado.id === id;
+  const empregadoInformado = data.employees.find(buscarEmpregado);
+  const idAnimalPeloQualEhResponsavel = empregadoInformado.responsibleFor[0];
+  const buscarObjAnimal = (animal) => animal.id === idAnimalPeloQualEhResponsavel;
+  const objAnimal = data.species.find(buscarObjAnimal);
+  const animaisResidentes = objAnimal.residents;
+  const buscarAnimalMaisVelho = (maior, atual) => {
+    if (atual.age > maior.age) { return atual; } return maior;
+  };
+  const animalResidenteMaisVelho = animaisResidentes.reduce(buscarAnimalMaisVelho);
+  const arrObjAnimalResidenteMaisVelho = [
+    Object.entries(animalResidenteMaisVelho)[0][1],
+    Object.entries(animalResidenteMaisVelho)[1][1],
+    Object.entries(animalResidenteMaisVelho)[2][1],
+  ];
+  return arrObjAnimalResidenteMaisVelho;
 }
 
 module.exports = getOldestFromFirstSpecies;
